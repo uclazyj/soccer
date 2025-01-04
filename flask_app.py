b1e3bbfa9b61b67f294a5a8a035f03a416f5c154
shdczyj <zhaoyujian94@gmail.com>
2025-01-04 16:32:33 +0000

    A simple in memory sign-up sheet

--- flask_app.py
+++ flask_app.py
@@ -3,9 +3,12 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-names = ["Alice", "Bob", "Carol"]
+names = []
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("index.html", names = names)
+    name = request.form["name"]
+    names.append(name)
+    return redirect(url_for('index'))
