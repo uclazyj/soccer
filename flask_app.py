9a872dfcdf29f5dc1da08053cb335ece61d534c2
shdczyj <zhaoyujian94@gmail.com>
2025-01-05 16:45:53 +0000

    Make repeated signup or withdraw idempotent

--- flask_app.py
+++ flask_app.py
@@ -15,25 +15,28 @@ date = "2025-01-01"
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        rows = db.execute("SELECT date, name, ts, cancel FROM signup")
-        return render_template("index.html", rows = rows)
+        rows_signup = db.execute("SELECT date, name, ts, cancel FROM signup WHERE cancel = 0 ORDER BY ts")
+        rows_withdraw = db.execute("SELECT date, name, ts, cancel FROM signup WHERE cancel = 1 ORDER BY ts")
+        return render_template("index.html", rows_signup = rows_signup, rows_withdraw = rows_withdraw)
     name = request.form.get("name")
+    if name == "" or name[0] == " ":
+        return redirect(url_for('index'))
+
     now = datetime.now(timezone('EST'))
     ts = now.strftime("%Y-%m-%d %H:%M:%S")
 
     submit_action = request.form.get('submitAction')
-    row = db.execute("SELECT COUNT(*) AS COUNT FROM signup WHERE date = ? AND name = ?", date, name)
-    count = row[0]["COUNT"]
+
+    row = db.execute("SELECT * FROM signup WHERE date = ? AND name = ?", date, name)
+
     if submit_action == "signup":
-        if count == 0:
+        if len(row) == 0:
             db.execute("INSERT INTO signup (date, name, ts, cancel) VALUES(?,?,?,?)", date, name, ts, 0)
-        else:
-            # TODO: Don't update ts if the user has already signed up. Only do so if the user decides to register again.
+        elif row[0]["cancel"] == 1:
             db.execute("UPDATE signup SET cancel = ?, ts = ? WHERE date = ? AND name = ? ",
                        0, ts, date, name)
     else:
-        if count == 1:
-            # Todo: Don't update ts if the user has already withdrawed.
+        if len(row) > 0 and row[0]["cancel"] == 0:
             db.execute("UPDATE signup SET cancel = ?, ts = ? WHERE date = ? AND name = ? ",
                        1, ts, date, name)
     return redirect(url_for('index'))
