8a15fcdd10479bafde27bd7b8be9e6c34a994ef7
shdczyj <zhaoyujian94@gmail.com>
2025-01-05 04:22:59 +0000

    Add withdraw button

--- flask_app.py
+++ flask_app.py
@@ -15,9 +15,25 @@ date = "2025-01-01"
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        rows = db.execute("SELECT date, name, signup_ts FROM signup")
+        rows = db.execute("SELECT date, name, ts, cancel FROM signup")
         return render_template("index.html", rows = rows)
-    name = request.form["name"]
+    name = request.form.get("name")
     now = datetime.now(timezone('EST'))
-    db.execute("INSERT INTO signup (date, name, signup_ts, cancel) VALUES(?,?,?,?)", date, name, now.strftime("%Y-%m-%d %H:%M:%S"), 0)
+    ts = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    submit_action = request.form.get('submitAction')
+    row = db.execute("SELECT COUNT(*) AS COUNT FROM signup WHERE date = ? AND name = ?", date, name)
+    count = row[0]["COUNT"]
+    if submit_action == "signup":
+        if count == 0:
+            db.execute("INSERT INTO signup (date, name, ts, cancel) VALUES(?,?,?,?)", date, name, ts, 0)
+        else:
+            # TODO: Don't update ts if the user has already signed up. Only do so if the user decides to register again.
+            db.execute("UPDATE signup SET cancel = ?, ts = ? WHERE date = ? AND name = ? ",
+                       0, ts, date, name)
+    else:
+        if count == 1:
+            # Todo: Don't update ts if the user has already withdrawed.
+            db.execute("UPDATE signup SET cancel = ?, ts = ? WHERE date = ? AND name = ? ",
+                       1, ts, date, name)
     return redirect(url_for('index'))
