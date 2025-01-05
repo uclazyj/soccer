52090b2e264177bae589f2821aa4d8bf54d12c3b
shdczyj <zhaoyujian94@gmail.com>
2025-01-05 01:07:58 +0000

    Use sqlite3 to store sign-up sheet

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,17 @@
 from flask import Flask, redirect, render_template, request, url_for
+from cs50 import SQL
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-names = []
+
+db = SQL("sqlite:////home/zhaoyujian/soccer/soccer.db")
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
+        names = db.execute("SELECT name FROM signup_nameonly")
         return render_template("index.html", names = names)
     name = request.form["name"]
-    names.append(name)
+    db.execute("INSERT INTO signup_nameonly (name) VALUES(?)", name)
     return redirect(url_for('index'))
